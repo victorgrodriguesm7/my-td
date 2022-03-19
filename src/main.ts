9ec778fba88c3b1cabfcd8b38c5df1f755e27e7d
victorgrodriguesm7 <victorgrodriguesm7@gmail.com>
2022-03-19 11:47:58 -0300

    feat: add engine basic example

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,31 @@
-console.log("Hello World")
+import Engine from "./core/engine";
+
+const canvas = document.querySelector("canvas");
+
+
+class Test implements Component {
+    x: number  = 0;
+    y: number  = 0;
+    width: number = 10;
+    height: number = 10;
+
+    update({ context, screenSize: { width } }: RenderProps): void {
+        context.fillRect(
+            this.x, this.y, this.width, this.height
+        )
+
+        this.x += 2;
+
+        if (this.x > width){
+            this.x = 0;
+        }
+    }
+}
+
+if (canvas){
+    const engine = new Engine(canvas);
+
+    engine.components.push(new Test())
+
+    engine.run();
+}

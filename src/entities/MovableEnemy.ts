68a73a80b5d4554d6d7097232e60f39a64f47805
victorgrodriguesm7 <victorgrodriguesm7@gmail.com>
2022-03-27 22:30:05 -0300

    feat: create movable enemy for test purposes

---
+++ src/entities/MovableEnemy.ts
@@ -0,0 +1,46 @@
+import InputHandler, { ListenerData } from "../core/input";
+import Enemy, { DefaultEnemyProps } from "./Enemy";
+
+interface MovableEnemyProps extends DefaultEnemyProps {
+
+}
+
+export default class MovableEnemy extends Enemy {
+    constructor({
+        width,
+        height,
+        x,
+        y
+    }: MovableEnemyProps){
+        super({
+            width,
+            height,
+            x,
+            y
+        });
+
+        const inputHandler = InputHandler.instance;
+
+        inputHandler.addListener({
+            event: "click",
+            callback: this.onClick,
+            component: this
+        });
+    }
+
+    onClick(props: ListenerData){
+        if (typeof props.x !== "undefined" && typeof props.y !== "undefined"){
+            this.x = props.x;
+            this.y = props.y
+        }
+    }
+
+    update({ context }: RenderProps): void {
+        context.fillRect(
+            this.x,
+            this.y,
+            this.width,
+            this.height
+        );
+    }
+}

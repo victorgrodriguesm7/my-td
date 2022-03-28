90193873563c98eb1f8cf3c730ceea24c379e5e6
victorgrodriguesm7 <victorgrodriguesm7@gmail.com>
2022-03-27 22:29:40 -0300

    feat: add first turret type

---
+++ src/entities/LightTurret.ts
@@ -0,0 +1,82 @@
+import Turret from "./Turret";
+
+interface LightTurretProps {
+    x: number;
+    y: number;
+}
+
+export default class LightTurret extends Turret {
+    i = 0;
+    constructor({
+        x,
+        y        
+    }: LightTurretProps){
+        super({
+            x,
+            y,
+            height: 16,
+            width: 16,
+            radius: 100,
+            firerate: 10,
+        });
+
+        this.targetAngle = 180;
+    }
+
+    draw(context: RenderProps["context"]){
+        context.fillStyle = "#0F0";
+
+        const sprite = document.createElement("img");
+
+        sprite.src = "/light.png";
+        
+        context.fillRect(
+            this.x,
+            this.y,
+            this.width,
+            this.height
+        );
+
+        context.save();
+
+        context.translate(
+            this.x + this.width / 2,
+            this.y + this.height /2
+        );
+        
+        context.rotate(this.currentAngle * Math.PI / 180);
+
+        context.translate(
+            - this.x - this.width / 2,
+            - this.y - this.height / 2
+        );
+
+        context.drawImage(
+            sprite,
+            this.x,
+            this.y
+        );
+
+        context.restore();
+
+        this.i++;
+    }
+
+    update(props: RenderProps): void {
+        const { context, components, screenSize } = props;
+
+        if (this.canShoot()){
+            const target = this.findNearestEnemy(components);
+
+            if (target){
+                const { enemy } = target;
+
+                this.findAngle(enemy, context, screenSize);
+                this.aim();
+            }
+        }
+        // this.showRadius(props.context);
+
+        this.draw(context);
+    }
+}
